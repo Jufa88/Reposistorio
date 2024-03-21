6bfd410ed9fc28c39e25d7f013b89d97ec0a5eef
Jufa88 <juanfranmargo8@gmail.com>
2024-03-21 09:35:54 +0100

    first commit

---
+++ Casa.java
@@ -0,0 +1,92 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Casa {
+
+    /**
+     * Default constructor
+     */
+    public Casa() {
+    }
+
+    /**
+     * 
+     */
+    public String direccion;
+
+    /**
+     * 
+     */
+    public int numHabitaciones;
+
+    /**
+     * 
+     */
+    public int metrosCuadrados;
+
+    /**
+     * @param direccion 
+     * @param numHabitaciones 
+     * @param metrosCuadrados
+     */
+    public void Casa(String direccion, int numHabitaciones, int metrosCuadrados) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getDireccion() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public int getNumHabitaciones() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public int getMetrosCuadrados() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param Direccion
+     */
+    public void setDireccion(String Direccion) {
+        // TODO implement here
+    }
+
+    /**
+     * @param numHabitaciones
+     */
+    public void setNumHabitaciones(int numHabitaciones) {
+        // TODO implement here
+    }
+
+    /**
+     * @param metrosCuadrados
+     */
+    public void setMetrosCuadrados(int metrosCuadrados) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String toString() {
+        // TODO implement here
+        return "";
+    }
+
+}
